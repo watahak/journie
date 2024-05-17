d344ccb5c97549f379e4ad19183fde029a96bcea
Ng Ping <ngping@zumvet.com>
2024-05-17 17:22:14 +0800

    fix: animation

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -8,10 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
       },
     },
   },
