cc3c1f1818db61ef9867c64b502ef819ec596d02
Ng Ping <ngping@zumvet.com>
2024-05-18 17:04:27 +0800

    fix: video access

--- web/app/components/genie.tsx
+++ web/app/components/genie.tsx
@@ -3,7 +3,7 @@ export default function Genie({ size }: { size: "sm" | "lg" }) {
 
   return (
     <video width={width} height="240" preload="none" loop={true} autoPlay muted playsInline>
-      <source src="https://storage.cloud.google.com/journie/assets/genie.mov" type="video/mp4" />
+      <source src="https://storage.googleapis.com/journie/assets/genie.mov" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
