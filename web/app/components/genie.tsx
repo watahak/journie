913422992d923169d905c1971b87e595b9053816
Ng Ping <ngping@zumvet.com>
2024-05-18 16:45:26 +0800

    fix: video

--- web/app/components/genie.tsx
+++ web/app/components/genie.tsx
@@ -3,7 +3,7 @@ export default function Genie({ size }: { size: "sm" | "lg" }) {
 
   return (
     <video width={width} height="240" preload="none" loop={true} autoPlay muted playsInline>
-      <source src="https://mt5gkfie7qymwe4f.public.blob.vercel-storage.com/genie-lwmVPIo3crhQ0ldlrSkHgoS9E79IAd.mov" type="video/mp4" />
+      <source src="https://storage.cloud.google.com/journie/assets/genie.mov" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
